c61658c665f5be7a737a40ad29df465a58de63dc
michaelrm27 <michaelmarcelino55@gmail.com>
2023-11-14 18:42:35 +0700

    style: change navbar styling

--- src/components/ui/navbar.jsx
+++ src/components/ui/navbar.jsx
@@ -1,25 +1,24 @@
-import { Button } from "./button";
+import { Button, buttonVariants } from "./button";
+import HarsaLogo from "../../assets/HarsaLogo.png";
 
 export default function Navbar() {
   return (
     <div>
-      <div className="bg-gray-300 flex justify-between ps-28 pe-28 pt-5 pb-5 items-center">
+      <div className="bg-[#fff] flex justify-between ps-2 pe-5 pt-5 pb-5 items-center">
         <div>
-          <p className="text-2xl">LOGO</p>
+          <img
+            src={HarsaLogo}
+            alt="HarsaLogo"
+          />
         </div>
-        <div className="flex text-2xl gap-10 items-center">
+        <div className="flex text-xl gap-16 items-center font-semibold text-[#092c4c]">
           <a href="">Home</a>
           <a href="">About Us</a>
           <a href="">Our Features</a>
           <a href="">Testimoni</a>
-          <a href="">Get App</a>
           <a href="">FAQ</a>
-          <Button
-            variant="secondary"
-            size="lg"
-          >
-            Login
-          </Button>
+          <a href="">Get App</a>
+          <Button className="bg-[#092C4C] w-36 h-10 font-bold">Login</Button>
         </div>
       </div>
     </div>
