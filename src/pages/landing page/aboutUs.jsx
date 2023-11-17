3858f27dc2a9789ebcb60b3d6894bfc07baa2530
Fanny Rahmasari <96949428+fannyrahmasari@users.noreply.github.com>
2023-11-17 16:02:53 +0700

    feat: add aboutUs

--- src/pages/landing page/aboutUs.jsx
+++ src/pages/landing page/aboutUs.jsx
@@ -2,17 +2,17 @@ import React from "react"
 import aboutImage from '../../assets/image/about.png'
 const AboutUs = () => {
     return(
-        <>
-        <div className="aboutUs container mx-auto flex">
-            <div>
-                <img src={aboutImage} alt="" />
-            </div>
-            <div>
-                <h2 className="font-i">About Us</h2>
-                <p>Harsa.Edu adalah aplikasi inovatif yang dirancang untuk memberikan pengalaman pembelajaran IT yang komprehensif dan terintegrasi. Platform ini tidak hanya menyediakan kurikulum pembelajaran yang lengkap, tetapi juga menyertakan fitur-fitur canggih yang mempermudah pengelolaan tugas, ujian, dan membantu Anda menjawab pertanyaan melalui integrasi dengan Chat AI.</p>
+        <div className="aboutUs container mx-auto">
+            <div className="flex flex-col md:flex-row justify-center items-center">
+                <div className="w-[300px] md:w-[500px]">
+                    <img src={aboutImage} alt="image" className="" />
+                </div>
+                <div className="w-[400px] md:w-[600px]">
+                    <h2 className="text-left font-inter font-semibold text-[20px] md:text-[30px] mb-2">About Us</h2>
+                    <p className=" text-justify font-poppins text-[10px] md:text-[16px]">Harsa.Edu adalah aplikasi inovatif yang dirancang untuk memberikan pengalaman pembelajaran IT yang komprehensif dan terintegrasi. Platform ini tidak hanya menyediakan kurikulum pembelajaran yang lengkap, tetapi juga menyertakan fitur-fitur canggih yang mempermudah pengelolaan tugas, ujian, dan membantu Anda menjawab pertanyaan melalui integrasi dengan Chat AI.</p>
+                </div>
             </div>
         </div>
-        </>
     )
 }
 
