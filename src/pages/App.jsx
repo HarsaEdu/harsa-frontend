7770707343ac4540d3bd7aa5213dffde713d9307
dimdimah <dimahluthfi15@gmail.com>
2023-11-17 14:34:27 +0700

    feat: adding FAQ section

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,37 +1,50 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '../assets/vite.svg'
-import '../styles/App.css'
-import { Button } from '@/components/ui/button'
-import { buttonVariants } from "@/components/ui/button"
-import { Link } from 'lucide-react'
+import { Button } from "@/components/ui/button";
+import LandingPages from "./landingPage/Faq";
+import reactLogo from "../assets/react.svg";
+import viteLogo from "../assets/vite.svg";
+import { useState } from "react";
+import "../styles/index.css";
 
 function App() {
   const [count, setCount] = useState(0);
   return (
-    <>
+    <div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <Button className="bg-slate-600 mx-3" onClick={() => setCount((count) => count + 1)} disabled>count is {count}</Button>
-        <Button className="bg-slate-600 mx-3" onClick={() => setCount((count) => count + 1)} >count is {count}</Button>
+        <Button
+          className="mx-3 bg-slate-600"
+          onClick={() => setCount((count) => count + 1)}
+          disabled
+        >
+          count is {count}
+        </Button>
+        <Button
+          className="mx-3 bg-slate-600"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </Button>
         <Button variant="outline">Button outline</Button>
-        <Button variant="link"><a href="/aaa">Link Btn</a></Button>
+        <Button variant="link">
+          <a href="/aaa">Link Btn</a>
+        </Button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      <LandingPages />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   );
 }
 
