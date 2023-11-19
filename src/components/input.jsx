7593c6b892e8f5797367092b00ac7ad52d86e547
dimdimah <dimahluthfi15@gmail.com>
2023-11-19 10:51:42 +0700

    fix: changing color bg & font

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import clsx from "clsx";
 
 function Input(props) {
--- src/pages/landingPage/Faq.jsx
+++ src/pages/landingPage/Faq.jsx
@@ -7,42 +7,52 @@ import {
 
 function Faq() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h2 className="mx-auto scroll-m-20 pb-2 text-2xl font-semibold">FAQ</h2>
-      <Accordion type="single" collapsible className="w-full">
-        <AccordionItem value="item-1">
-          <AccordionTrigger>BAGAIMANA CARA MENDOWNLOAD HARSA?</AccordionTrigger>
-          <AccordionContent>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-            auctor ante quis risus posuere, ut vulputate sem tincidunt. Sed
-            varius augue in justo lacinia, in dictum leo rhoncus. Integer
-            lacinia nisi a libero.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger>BAGAIMANA CARA MENDOWNLOAD HARSA?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It comes with default styles that matches the other
-            components&apos; aesthetic.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-3">
-          <AccordionTrigger>BAGAIMANA CARA MENDOWNLOAD HARSA?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It{"'"}s animated by default, but you can disable it if you
-            prefer.
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-4">
-          <AccordionTrigger>BAGAIMANA CARA MENDOWNLOAD HARSA?</AccordionTrigger>
-          <AccordionContent>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-            auctor ante quis risus posuere, ut vulputate sem tincidunt. Sed
-            varius augue in justo lacinia, in dictum leo rhoncus. Integer
-            lacinia nisi a libero.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+    <div className="bg-[#092C4C] text-[#FFFFFF]">
+      <div className="mx-10 flex flex-col items-center justify-center py-5">
+        <h2 className="mx-auto scroll-m-20 pb-2 text-xl font-semibold">FAQ</h2>
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>
+              BAGAIMANA CARA MENDOWNLOAD HARSA?
+            </AccordionTrigger>
+            <AccordionContent>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+              auctor ante quis risus posuere, ut vulputate sem tincidunt. Sed
+              varius augue in justo lacinia, in dictum leo rhoncus. Integer
+              lacinia nisi a libero.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>
+              BAGAIMANA CARA MENDOWNLOAD HARSA?
+            </AccordionTrigger>
+            <AccordionContent>
+              Yes. It comes with default styles that matches the other
+              components&apos; aesthetic.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>
+              BAGAIMANA CARA MENDOWNLOAD HARSA?
+            </AccordionTrigger>
+            <AccordionContent>
+              Yes. It{"'"}s animated by default, but you can disable it if you
+              prefer.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-4">
+            <AccordionTrigger>
+              BAGAIMANA CARA MENDOWNLOAD HARSA?
+            </AccordionTrigger>
+            <AccordionContent>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+              auctor ante quis risus posuere, ut vulputate sem tincidunt. Sed
+              varius augue in justo lacinia, in dictum leo rhoncus. Integer
+              lacinia nisi a libero.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </div>
   );
 }
