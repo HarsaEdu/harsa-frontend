996f4fcffe24ec9fbabf29b6e4cd68828bb4598f
cha-chaham <charliech1056@gmail.com>
2023-11-19 11:19:42 +0800

    feat: resolve layerbase index.css

--- src/components/landing-page/hero.jsx
+++ src/components/landing-page/hero.jsx
@@ -1,37 +1,44 @@
-import React from 'react'
-import { Button } from '../ui/button'
-import heroImage from '@/assets/hero-img.png'
+import React from "react";
+import { Button } from "../ui/button";
+import heroImage from "@/assets/hero-img.png";
 
 export default function Hero() {
   return (
     <div
-          className="flex flex-row py-24 items-center px-20 justify-between"
-          id="beranda"
-        >
-          <div className="text-center md:text-start w-1/2">
-            <p className="font-bold text-[#092C4C] text-5xl leading-tight">
-            Transformasi Pembelajaran <br /> Melalui Platform LMS Unggulan
-            </p>
-            <div className="xl:px-16 w-full flex mt-12 xl:text-xl mb-6">
-              <div>
-                  <div className='font-semibold text-[#092C4C] list-item xl:xl:w-56 mb-3 mr-20'>Dipercaya oleh lebih dari +200 institusi</div>
-                  <div className='font-semibold text-[#092C4C] list-item xl:w-56 mb-3 mr-20'>Kemudahan dalam akses pembelajaran</div>
-              </div>
-              <div>
-                  <div className='font-semibold text-[#092C4C] list-item xl:w-60 mb-3 mr-20'>LMS pertama yang mengimplementasikan AI</div>
-                  <div className='font-semibold text-[#092C4C] list-item xl:w-56 mb-3 mr-20'>Kemudahan dalam akses pembelajaran</div>
-              </div>
+      className="flex flex-row items-center justify-between px-20 py-24"
+      id="beranda"
+    >
+      <div className="w-3/4 text-center md:text-start">
+        <p className="text-5xl font-bold leading-tight text-[#092C4C]">
+          Transformasi Pembelajaran <br /> Melalui Platform LMS Unggulan
+        </p>
+        <div className="mb-6 mt-12 flex w-full xl:px-16 xl:text-xl">
+          <div>
+            <div className="mb-3 mr-20 list-item font-semibold text-[#092C4C] xl:xl:w-56">
+              Dipercaya oleh lebih dari +200 institusi
             </div>
-            <a href="#emailAction">
-              <Button
-                
-                className="bg-[#092C4C] text-white font-semibold text-lg py-6 rounded-lg hover:bg-[#092C4C] w-72"
-              >Unduh Sekarang</Button>
-            </a>
+            <div className="mb-3 mr-20 list-item font-semibold text-[#092C4C] xl:w-56">
+              Kemudahan dalam akses pembelajaran
+            </div>
           </div>
-          <div className="hidden md:flex md:justify-center">
-            <img src={heroImage} alt="" className='h-92'/>
+          <div>
+            <div className="mb-3 mr-20 list-item font-semibold text-[#092C4C] xl:w-60">
+              LMS pertama yang mengimplementasikan AI
+            </div>
+            <div className="mb-3 mr-20 list-item font-semibold text-[#092C4C] xl:w-56">
+              Kemudahan dalam akses pembelajaran
+            </div>
           </div>
         </div>
-  )
+        <a href="#emailAction">
+          <Button className="w-72 bg-[#092C4C] py-6 text-lg font-semibold text-white">
+            Unduh Sekarang
+          </Button>
+        </a>
+      </div>
+      <div className="hidden w-1/2 md:flex md:justify-center">
+        <img src={heroImage} alt="" className="object-contain" />
+      </div>
+    </div>
+  );
 }
