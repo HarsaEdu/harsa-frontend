048b4418826a2c483a72211ff2c08ef3908ddbc3
Fanny Rahmasari <96949428+fannyrahmasari@users.noreply.github.com>
2023-11-20 16:27:12 +0700

    feat: update layout

--- src/components/layout/Index.jsx
+++ src/components/layout/Index.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import Sidebar from './Sidebar';
-import Content from './Content';
 
 const Layout = (props) => {
-  const { className } = props
+    const {children, className} = props
 
   return (
-    <div className={`flex ${className}`}>
-        <Sidebar></Sidebar>
-        <Content></Content>
+    <div className="flex">
+      {/* Sidebar */}
+      <div className={`bg-gray-400 ${className} h-screen w-1/5 p-4`}>
+        {children}
+        <p>Sidebar Content</p>
+      </div>
+
+      {/* Content */}
+      <div className={`bg-gray-200 ${className} h-screen w-4/5 p-4`}>
+        {children}
+        <p>Main Content</p>
+      </div>
     </div>
   );
 };
