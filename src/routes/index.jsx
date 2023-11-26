bc7153a5702bd8659ad599dca7a30ce40c4574ea
KokoYuardiA <92006583+KokoYuardiA@users.noreply.github.com>
2023-11-27 01:45:57 +0800

    fix: fixing routes and breadcrumb

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,24 +2,23 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "../pages/App";
 import LandingPage from "../pages/landing-page/index";
-import AboutUs from "@/pages/landing page/aboutUs";
+import Dashboard from "@/pages/dashboard/index";
+import ListClass from "@/pages/list-class";
 
 
 export default function Router() {
     const router = createBrowserRouter([
         {
             path: '/',
-            element: <App />,
+            element: <LandingPage />,
         },
         {
-            path: '/landing-page',
-            element: <LandingPage />,
-            children: [
-                {
-                    path: 'kelas', // Menggunakan path yang terpisah untuk Kelas
-                    element: <AboutUs />
-                },
-            ],
+            path: '/dashboard',
+            element: <Dashboard />,
+        },
+        {
+            path: '/kelas',
+            element: <ListClass />
         },
     ])
 
