b0b30e3858e0b8d90f87afb1615d5b730ba6ece3
Fanny Rahmasari <96949428+fannyrahmasari@users.noreply.github.com>
2023-11-28 22:43:51 +0700

    feat: add tambah kuis

--- src/pages/tambah-kuis/index.jsx
+++ src/pages/tambah-kuis/index.jsx
@@ -5,24 +5,26 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Link } from "react-router-dom"
 
-import Plus from "../../assets/Plus.svg"
-import File from "../../assets/File.svg"
-import Bell from "../../assets/bell.svg"
+import PlusIcon from "../../assets/Plus.svg"
+import FileIcon from "../../assets/File.svg"
+import BellIcon from "../../assets/bell.svg"
 
 const TambahKuis = () => {
     return(
-        <Layout>
+        <Layout
+        className="font-poppins" 
+        >
             <div className="flex justify-end">
-                <img src={Bell} alt="Icon" className="w-[50px]" />
+                <img src={BellIcon} alt="Icon" className="w-[50px]" />
             </div>
             <div className="flex">
                 <Breadcrumb />
-                <Link To="#" className="font-poppins">Kelas &gt;</Link>
-                <Link To="#" className="font-poppins">Tambah Kuis</Link>
+                <Link To="#">Kelas &gt;</Link>
+                <Link To="#">Tambah Kuis</Link>
             </div>
             <div className="flex justify-between items-center mt-3">
                 <div className="flex flex-row justify-center items-center">
-                    <h2 className="font-poppins mr-2 text-[#092C4C]">Search</h2>
+                    <h2 className="mr-2 text-[#092C4C]">Search</h2>
                     <Input
                     className="w-[200px] border-[#092C4C] rounded-[12px]" 
                     />
@@ -30,14 +32,14 @@ const TambahKuis = () => {
                 <div>
                     <Button>
                         Tambah Kuis 
-                        <img src={Plus} alt="Icon" className="w-[16px] ml-2"/>
+                        <img src={PlusIcon} alt="Icon" className="w-[16px] ml-2"/>
                     </Button>
                 </div>
             </div>
 
             <div className="flex flex-col justify-center items-center mt-20">
-                <h3 className="text-[#999999] font-poppins">Belum ada kuis yang dibuat</h3>
-                <img src={File} alt="Icon" className="w-[198px]"/>
+                <h3 className="text-[#999999] ">Belum ada kuis yang dibuat</h3>
+                <img src={FileIcon} alt="Icon" className="w-[198px]"/>
             </div> 
         </Layout>
     )
