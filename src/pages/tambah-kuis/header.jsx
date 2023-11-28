6916399922a207c65ed2043b52607b6d760502c8
cha-chaham <charliech1056@gmail.com>
2023-11-28 23:50:48 +0800

    feat: add halaman jawaban kuis

---
+++ src/pages/tambah-kuis/header.jsx
@@ -0,0 +1,53 @@
+import Breadcrumb from "@/components/breadcrumb";
+import { React, useState } from "react";
+import JawabanKuis from "./jawabanKuis";
+
+export default function HeaderQuiz() {
+  const [isActivePertanyaan, setActivePertanyaan] = useState(true);
+
+  const handlePertanyaanClick = () => {
+    setActivePertanyaan(true);
+  };
+
+  const handleJawabanClick = () => {
+    setActivePertanyaan(false);
+  };
+
+  return (
+    <div>
+      <Breadcrumb />
+
+      <div className="mt-12 flex justify-center">
+        <div className="flex flex-col items-center">
+          <h4 className="text-4xl font-bold text-[#092C4C]">Kuis</h4>
+
+          {/* State Render Component */}
+          <div className="mt-8 flex space-x-12">
+            <h4
+              className={`cursor-pointer text-3xl font-bold ${
+                isActivePertanyaan
+                  ? "border-b-2 border-b-black pb-2 text-[#092C4C]"
+                  : "text-[#A2D2FF]"
+              }`}
+              onClick={handlePertanyaanClick}
+            >
+              Pertanyaan
+            </h4>
+            <h4
+              className={`cursor-pointer text-3xl font-bold ${
+                !isActivePertanyaan
+                  ? "border-b-2 border-b-black pb-2 text-[#092C4C]"
+                  : "text-[#A2D2FF]"
+              }`}
+              onClick={handleJawabanClick}
+            >
+              Jawaban
+            </h4>
+          </div>
+        </div>
+      </div>
+
+      <div>{isActivePertanyaan ? null : <JawabanKuis />}</div>
+    </div>
+  );
+}
