ece2cf5f4a7dc2324123d514ac04770fb92df6a5
Syifa Zahwa <syifazahwa.szf88@gmail.com>
2023-12-04 16:46:45 +0700

    feat: add dropzone

--- src/pages/kelas/Formkelas.jsx
+++ src/pages/kelas/Formkelas.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
@@ -11,12 +12,72 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { FaUpload, FaTimes } from "react-icons/fa";
 
 const formSchema = z.object({
   judul: z.string().nonempty("*Judul wajib di isi"),
-  deskripsi: z.string().nonempty("*Deskripsi wajib di isi "),
-  option: z.string().nonempty("*Kategori wajib di pilih "),
+  deskripsi: z.string().nonempty("*Deskripsi wajib di isi"),
+  option: z.string().nonempty("*Kategori wajib di pilih"),
+  uploads: z.string().nonempty("*Gambar wajib di pilih"),
 });
+
+const DropzoneComponent = ({ setPreview, setValue }) => {
+  const [file, setFile] = useState(null);
+
+  const handleImageChange = (newFile) => {
+    if (newFile) {
+      setPreview(URL.createObjectURL(newFile));
+      setFile(newFile);
+      setValue("uploads", newFile);
+    } else {
+      setPreview("");
+      setFile(null);
+      setValue("uploads", "");
+    }
+  };
+
+  const removeImage = () => {
+    setPreview("");
+    setFile(null);
+    setValue("uploads", "");
+  };
+
+  return (
+    <div className="my-5 text-center relative">
+      {file ? (
+        <>
+          <img
+            src={URL.createObjectURL(file)}
+            alt="Cover Preview"
+            className="rounded-lg border border-[#092C4C] max-h-40 object-cover mx-auto"
+          />
+          <button
+            className="absolute top-0 right-0 m-2 bg-white rounded-full p-1 cursor-pointer"
+            onClick={removeImage}
+          >
+            <FaTimes size={16} color="#092C4C" />
+          </button>
+        </>
+      ) : (
+        <label
+          htmlFor="upload"
+          className="cursor-pointer flex flex-col items-center border rounded-lg border-[#092C4C] p-8"
+        >
+          <FaUpload size={40} className="mx-auto text-[#092C4C]" />
+          <div className="mt-1">Upload Cover Course</div>
+        </label>
+      )}
+      <input
+        type="file"
+        id="upload"
+        style={{ display: "none" }}
+        onChange={(e) => {
+          handleImageChange(e.target.files[0]);
+        }}
+      />
+    </div>
+  );
+};
 
 export default function FormKelas() {
   const form = useForm({
@@ -28,14 +89,25 @@ export default function FormKelas() {
     },
   });
 
-  const onSubmit = (data) => {
-    alert("Sukses Tambah Kelas");
+  const [preview, setPreview] = useState("");
+  const [showAlert, setShowAlert] = useState(false);
+
+  const onSubmit = async (data) => {
+    setShowAlert(true);
+  };
+
+  const handleAlertClose = () => {
+    setShowAlert(false);
+  };
+
+  const handleCancel = () => {
     form.reset();
+    setPreview("");
   };
 
   return (
     <main className="bg-[#FFFFFF]">
-      <section className="mx-auto h-[650px] max-w-[1000px] rounded-lg border border-[#092C4C] p-4">
+      <section className={`mx-auto max-w-[800px] rounded-lg border border-[#092C4C] p-8 relative overflow-hidden ${showAlert ? "alert-open" : ""}`}>
         <h1 className="mb-5 text-2xl font-bold text-[#092C4C]">Tambah Kelas</h1>
         <Form {...form}>
           <form
@@ -50,7 +122,7 @@ export default function FormKelas() {
                 <Input
                   type="text"
                   placeholder="Judul Kelas"
-                  className="h-[40px] w-[850px] rounded-s border border-[#092C4C] bg-transparent px-3 py-4 text-black outline-none focus-within:border-[#092C4C] hover:border-[#092C4C]"
+                  className="h-[40px] w-[710px] rounded-s border border-[#092C4C] bg-transparent px-3 py-4 text-black outline-none focus-within:border-[#092C4C] hover:border-[#092C4C]"
                   {...form.register("judul")}
                 />
               </FormControl>
@@ -66,7 +138,7 @@ export default function FormKelas() {
               <FormControl>
                 <textarea
                   placeholder="Deskripsi"
-                  className="h-[175px] w-[850px] resize-none rounded-lg border border-[#092C4C] bg-transparent px-3 py-4 text-black outline-none focus-within:border-[#092C4C] hover:border-[#092C4C]"
+                  className="h-[175px] w-[710px] resize-none rounded-lg border border-[#092C4C] bg-transparent px-3 py-4 text-black outline-none focus-within:border-[#092C4C] hover:border-[#092C4C]"
                   {...form.register("deskripsi")}
                 />
               </FormControl>
@@ -81,7 +153,7 @@ export default function FormKelas() {
               </FormLabel>
               <FormControl>
                 <select
-                  className="flex h-[40px] w-[850px] rounded-lg border border-[#092C4C] bg-transparent px-2 py-2 text-black outline-none focus-within:border-[#092C4C] hover:border-[#092C4C]"
+                  className="flex h-[40px] w-[710px] rounded-lg border border-[#092C4C] bg-transparent px-2 py-2 text-black outline-none focus-within:border-[#092C4C] hover:border-[#092C4C]"
                   {...form.register("option")}
                 >
                   <option value="" disabled>
@@ -98,23 +170,45 @@ export default function FormKelas() {
               </FormMessage>
             </FormItem>
 
+            <FormItem>
+              <FormLabel className="ml-1 text-lg font-bold text-[#092C4C]">
+                Cover
+              </FormLabel>
+              <FormControl>
+                <DropzoneComponent
+                  setPreview={setPreview}
+                  setValue={form.setValue}
+                />
+              </FormControl>
+              <FormMessage className="text-[#ED7878]">
+                {form.formState.errors.uploads?.message}
+              </FormMessage>
+            </FormItem>
+
             <div className="flex justify-between">
               <Button
                 type="button"
-                onClick={() => form.reset()}
-                className="text-s ml-[45px] h-[40px] w-[150px] rounded-lg border border-[#092C4C] bg-[#FFFFFF] py-4 text-center font-bold text-[#092C4C] hover:bg-[#092C4C]/90 active:scale-95"
+                onClick={handleCancel}
+                className="text-s h-[40px] w-[150px] rounded-lg border border-[#092C4C] bg-[#FFFFFF] py-4 text-center font-bold text-[#092C4C] hover:bg-[#092C4C]/90 active:scale-95"
               >
                 Batal
               </Button>
               <Button
                 type="submit"
-                className="text-s mr-[80px] h-[40px] w-[150px] rounded-lg bg-[#092C4C] py-4 text-center font-bold text-white hover:bg-[#092C4C]/90 active:scale-95"
+                className="text-s h-[40px] w-[150px] rounded-lg bg-[#092C4C] py-4 text-center font-bold text-white hover:bg-[#092C4C]/90 active:scale-95"
               >
                 Simpan
               </Button>
             </div>
           </form>
         </Form>
+        {showAlert && (
+          <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+            <div className="bg-white p-4 rounded border border-[#092C4C]">
+              <button onClick={handleAlertClose}>Close</button>
+            </div>
+          </div>
+        )}
       </section>
     </main>
   );
