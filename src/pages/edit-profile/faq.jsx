52116dd3cebedc2ff01ec28524e44b573c913798
KokoYuardiA <92006583+KokoYuardiA@users.noreply.github.com>
2023-12-07 03:18:22 +0800

    feat: add integration for FAQ and add FAQ

--- src/pages/edit-profile/faq.jsx
+++ src/pages/edit-profile/faq.jsx
@@ -1,64 +1,53 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import axios from "axios";
 import EditProfileLayout from "@/components/layout/EditProfileLayout";
 
 export default function EditProfileFaq() {
+
+  const [faqData, setFaqData] = useState([]);
+
+  useEffect(() => {
+    // Mengambil data dari backend saat komponen dimuat
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.harsaedu.my.id/web/faqs?offset=0&limit=10"
+        );
+        const data = response.data.data;
+        setFaqData(data);
+      } catch (error) {
+        console.error("Error fetching FAQ data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <EditProfileLayout>
       <p className="font-bold text-2xl">FAQ</p>
       <p className="mt-10 font-bold text-xl">Frequently Ask Questions</p>
       <div className="mt-7 space-y-5">
-        <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="p-3 text-white rounded-t-lg rounded-b-none bg-[#092C4C] font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, molestiae!</AccordionTrigger>
-            <AccordionContent className="p-5 border border-[#092C4C] border-opacity-50 rounded-b-lg">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum dicta qui libero asperiores cupiditate corporis eligendi voluptas quisquam aspernatur repudiandae?
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-
-        <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="p-3 text-white rounded-t-lg rounded-b-none bg-[#092C4C] font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, molestiae!</AccordionTrigger>
-            <AccordionContent className="p-5 border border-[#092C4C] border-opacity-50 rounded-b-lg">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum dicta qui libero asperiores cupiditate corporis eligendi voluptas quisquam aspernatur repudiandae?
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-
-        <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="p-3 text-white rounded-t-lg rounded-b-none bg-[#092C4C] font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, molestiae!</AccordionTrigger>
-            <AccordionContent className="p-5 border border-[#092C4C] border-opacity-50 rounded-b-lg">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum dicta qui libero asperiores cupiditate corporis eligendi voluptas quisquam aspernatur repudiandae?
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-
-        <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="p-3 text-white rounded-t-lg rounded-b-none bg-[#092C4C] font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, molestiae!</AccordionTrigger>
-            <AccordionContent className="p-5 border border-[#092C4C] border-opacity-50 rounded-b-lg">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum dicta qui libero asperiores cupiditate corporis eligendi voluptas quisquam aspernatur repudiandae?
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-
-        <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger className="p-3 text-white rounded-t-lg rounded-b-none bg-[#092C4C] font-medium">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, molestiae!</AccordionTrigger>
-            <AccordionContent className="p-5 border border-[#092C4C] border-opacity-50 rounded-b-lg">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum dicta qui libero asperiores cupiditate corporis eligendi voluptas quisquam aspernatur repudiandae?
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      </div>
+          <Accordion type="single" collapsible className="w-full mt-7 space-y-5">
+            {faqData.map((faq) => (
+              <AccordionItem key={faq.id} value={`item-${faq.id}`}>
+                <AccordionTrigger className="p-3 text-white rounded-t-lg rounded-b-none bg-[#092C4C] font-medium">
+                  {faq.question}
+                </AccordionTrigger>
+                <AccordionContent className="p-5 border border-[#092C4C] border-opacity-50 rounded-b-lg">
+                  {faq.answer}
+                </AccordionContent>
+              </AccordionItem>
+            ))}
+          </Accordion>
+        </div>
       </EditProfileLayout>
     </div>
   );
