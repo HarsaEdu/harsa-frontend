e9c7bfe183229a39c3ac0561e71acd5657bebe7a
Syifa Zahwa <syifazahwa.szf88@gmail.com>
2023-12-07 06:41:59 +0700

    feat: add form tambah kelas

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,32 +1,13 @@
 import { React, useState } from "react";
 import Layout from "@/components/layout/Index";
-import Breadcrumb from "@/components/breadcrumb";
-import EditTugas from "./manage-tugas/editTugas";
-import InputFile from "@/components/inputFile";
-import MateriOverview from "./module/updateKelas";
+// import Breadcrumb from "@/components/breadcrumb";
+import Formkelasadmin from "./module/formKelasAdmin";
 
 function App() {
-  const [preview, setPreview] = useState("");
-  const handleImageChange = (file) => {
-    if (file) {
-      setPreview(URL.createObjectURL(file));
-    } else {
-      setPreview(""); // Reset the preview when image is deleted
-    }
-  };
+
   return (
     <div>
-      <InputFile
-        textUpload="Upload Cover"
-        preview={preview}
-        onChange={(e) => {
-          // field.onChange(e.target.files[0]);
-          // console.log(field);
-          handleImageChange(e.target.files[0]);
-        }}
-        setPreview={setPreview}
-      />
-      <MateriOverview />
+      <Formkelasadmin/>
     </div>
   );
 }
