c0a14c355e743942cc3e3f052b4e813ccdd87ee4
KokoYuardiA <92006583+KokoYuardiA@users.noreply.github.com>
2023-12-11 05:48:48 +0800

    fix: navigation on sidebar

--- src/components/Sidebar/DropdownConfig.jsx
+++ src/components/Sidebar/DropdownConfig.jsx
@@ -71,7 +71,7 @@ const InstructorIndex = () => {
             </Link>
           </AccordionContent>
           <AccordionContent className="group text-base pl-8">
-            <Link to="/content-management/FAQ/"
+            <Link to="/content-management/FAQ"
               className="text-md flex flex-row items-center gap-3 rounded-md px-2 py-2 text-[#FFFFFF] hover:bg-[#A2D2FF] hover:text-zinc-950"
             >
               <svg
