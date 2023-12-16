7739bcab0ebe8e85b88f2f1e2b439df4693a33ab
cha-chaham <charliech1056@gmail.com>
2023-12-16 15:06:35 +0800

    feat: integrasi jawban kuis, fixing table subs, fix list class func

--- src/utils/apis/subs-plan/api.js
+++ src/utils/apis/subs-plan/api.js
@@ -1,5 +1,30 @@
 import axiosWithConfig from "../axiosWithConfig";
-export const getSubs = async () => {
+export const getSubs = async (params) => {
+  try {
+    let query = "";
+
+    if (params) {
+      const queryParams = [];
+
+      for (const key in params) {
+        if (params.hasOwnProperty(key)) {
+          queryParams.push(`${key}=${params[key]}`);
+        }
+      }
+
+      query = queryParams.join("&");
+    }
+
+    const url = query ? `/subs-plan?&${query}` : "/subs-plan";
+
+    const response = await axiosWithConfig.get(url);
+
+    return response.data;
+  } catch (error) {
+    throw Error(error);
+  }
+};
+export const getMySubs = async () => {
   try {
     const response = await axiosWithConfig.get("/subs-plan");
 
@@ -8,18 +33,6 @@ export const getSubs = async () => {
     throw Error("Failed to get class");
   }
 };
-export const getMySubs = async () => {
-  try {
-    const response = await axiosWithConfig.get(
-      "/subs-plan",
-    );
-
-    return response.data;
-  } catch (error) {
-    throw Error("Failed to get class");
-  }
-};
-
 
 export const getDetailSubs = async (subsId) => {
   try {
@@ -29,28 +42,29 @@ export const getDetailSubs = async (subsId) => {
   } catch (error) {
     throw Error("Gagal mendapatkan detail kelas" + error);
   }
-}; 
-
+};
 
 export const createSubs = async (subsData) => {
-    try {
-      const response = await axiosWithConfig.post("/subs-plan", subsData);
-      return response.data;
-    } catch (error) {
-      throw Error("Failed to create subs");
-    }
+  try {
+    const response = await axiosWithConfig.post("/subs-plan", subsData);
+    return response.data;
+  } catch (error) {
+    throw Error("Failed to create subs");
+  }
 };
 
 export const updateSubs = async (subsId, subsData) => {
   try {
-    const response = await axiosWithConfig.put(`/subs-plan/${subsId}`, subsData);
+    const response = await axiosWithConfig.put(
+      `/subs-plan/${subsId}`,
+      subsData,
+    );
     return response.data;
   } catch (error) {
     throw new Error("Failed to update subs");
   }
 };
 
- 
 export const deleteSubs = async (subsId) => {
   try {
     const response = await axiosWithConfig.delete(`/subs-plan/${subsId}`);
